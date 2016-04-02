a4a861d36d7bd20966fe2c0f6fc3a7a979de53b7
Santiago Loane <santiago.loane@rochester.edu>
2016-04-02 12:25:06 -0400

    started some formulae for optimal naptime finding

--- calc.py
+++ calc.py
@@ -2,6 +2,15 @@
 #
 import math
 
+### IDEAL NAP TIMES (by priority)
+#
+# 90-120 min => full, deep, REM sleep
+#
+# 45 min => upper bounds on nap before 90 min. DO NOT go b/w 45 and 90 min
+#
+# 20 min=> power nap! Not fully resting, but makes you more alert & awake
+
+
 #boolean array of 5-minute intervals, tells whether or not this person is free at that time
 occupied = [288]
 
@@ -14,9 +23,16 @@ def is_free(occupied, nap_interval):
 	return freeness
 
 #converts hours to 5-minute intervals
+#assuming value between 0 and 24
 def hour_converter(hours):
 	return min_converter(hours*60)
 
 #converts minutes to 5-minute intervals
+#assuming value between 0 and 1440
 def min_converter(minutes):
 	return (minutes/5)
+
+#Find your ideal time of day for a nap! Depends on user's sleeping schedule
+def nap_zone(sleeptime, waketime):
+	normfactor = 51.2
+	goal = 4.67*((sleeptime+waketime)/2)-normfactor
